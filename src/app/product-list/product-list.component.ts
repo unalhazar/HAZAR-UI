e7a73bd1848396bbcf522b6dccf48daf5bca806d
Ünal Hazar <unalhazarr@gmail.com>
2024-08-27 17:44:55 +0300

    .

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,14 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { DefaultService } from '../../../api/default.service'; // Servisinizin doğru yolunu kontrol edin
-import { Product } from '../../../model/product'; // Modelin doğru yolunu kontrol edin
-import { CommonModule } from '@angular/common'; // Angular common modüllerini ithal edin
-import { HttpClientModule } from '@angular/common/http'; // HTTP için modül
+import { DefaultService } from '../../../api/default.service';
+import { Product } from '../../../model/product';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [CommonModule, HttpClientModule], // HttpClientModule'u buraya ekleyin
+  imports: [CommonModule],
   template: `
     <div *ngIf="products.length > 0">
       <h2>Product List</h2>
